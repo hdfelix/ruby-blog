6979e65421f303214055f803acb472f89a574b93
Hector D. Felix <hdfelix@gmail.com>
2014-02-09 17:29:12 -0800

    Initial commit for Rails tutorial code (http://guides.rubyonrails.org/getting_started.html), section 5.10.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '9e1038bb47319b10d396e43d5db9b2e652aefd8a69e429075a9c0048093fdd4c10239bbc2580ad1087fe2a6b8812fd83bbd79871985d8252abac43b7dac0a899'
